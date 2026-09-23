be1f51e3b9e234579d1570bc3fde59b0789cbdfd
JuanGualim <gua24852@uvg.edu.gt>
2026-09-23 01:03:25 -0600

    feat(transports): agrega fábrica createTransport y exportaciones
    Permite crear transportes a partir de configuración declarativa.

---
+++ src/transports/index.ts
@@ -0,0 +1,6 @@
+export * from './ChatTransport';
+export * from './MockTransport';
+export * from './WebSocketTransport';
+export * from './createTransport';
+export { defaultResponder, chunkText } from './mock/responder';
+export type { MockReply, MockResponder } from './mock/responder';
